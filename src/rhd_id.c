b2550475f62eff829e27cf32886e616750a03a20
Luc Verhaegen <libv@skynet.be>
2007-09-20 12:17:09 +0200

    ID: MacBook Pro 2.2
    Reported by Martin Aumueller.

--- src/rhd_id.c
+++ src/rhd_id.c
@@ -260,6 +260,13 @@ RHDIdentify(int flags)
     {RHD_CONNECTOR_VGA, "VGA", RHD_DDC_0, RHD_HPD_NONE, \
        { RHD_OUTPUT_DACA, RHD_OUTPUT_NONE}}}
 
+#define PANEL_B2_DVI_AA00 \
+  { {RHD_CONNECTOR_PANEL, "Panel", RHD_DDC_2, RHD_HPD_NONE, \
+       { RHD_OUTPUT_LVTMA, RHD_OUTPUT_NONE}}, \
+    {RHD_CONNECTOR_DVI, "DVI-I", RHD_DDC_0, RHD_HPD_0, \
+       { RHD_OUTPUT_DACA, RHD_OUTPUT_TMDSA}}}
+
+
 /*
  * List of pci subsystem / card ids.
  *
@@ -337,6 +344,8 @@ rhdCards[] =
                               { 1400, 1050, 0x8CA, 0x00F, 0x08, 0x2D, 0x3E8}},
     /* 0x71C5 : M56 : Mobility Radeon X1600 */
     { 0x71C5, 0x103C, 0x30B4, "HP/Compaq nw8440", PANEL_B1_VGA_A0,
+                              {    0,    0, 0xF9F, 0x00D, 0x03, 0x19, 0x1F4}},
+    { 0x71C5, 0x106B, 0x0080, "Macbook Pro 2,2", PANEL_B2_DVI_AA00,
                               {    0,    0, 0xF9F, 0x00D, 0x03, 0x19, 0x1F4}},
     /* 0x71C6 : RV530 : Radeon X1600 */
     /* 0x71C7 : RV535 : Radeon X1650 */
