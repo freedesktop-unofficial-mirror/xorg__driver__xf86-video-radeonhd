7713ffb0173587cae203154e9994b801e3e60884
Egbert Eich <eich@freedesktop.org>
2007-10-03 22:09:48 +0200

    Removing warning about unknown card.

--- src/rhd_driver.c
+++ src/rhd_driver.c
@@ -443,17 +443,6 @@ RHDPreInit(ScrnInfoPtr pScrn, int flags)
 	xf86DrvMsg(pScrn->scrnIndex, X_PROBED, "Detected an %s on an "
 		   "unidentified card\n", pScrn->chipset);
 
-    /* TODO: generic connector handling */
-    if (!rhdPtr->Card) {
-	xf86DrvMsg(pScrn->scrnIndex, X_ERROR,
-		   "Cannot map connectors on an unknown card!\n"
-		   "To help rectify this, please send your X log to\n\t\t"
-		   "radeonhd_at_opensuse.org.\n\t"
-		   "or file a bug for the radeonhd component\n\t"
-		   "of the X.Org product at bugs.freedesktop.org.\n"
-	    );
-    }
-
     /* We have none of these things yet. */
     rhdPtr->noAccel.val.bool = TRUE;
 
