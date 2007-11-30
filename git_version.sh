freedesktop-unofficial-mirror/xorg__driver__xf86-video-radeonhd

commit ff796eba071341aad9235520a53f2c33456447fe

Get rid of redundant broken GIT_DIR hack

git_version.sh
@@ -25,11 +25,10 @@ Options:
   -s, --srcdir DIRNAME   Set source tree dir name.
   -x, --example          Print complete example program."
 
-# The caller may have found these programs for us
-SED="${SED-sed}"
+# The caller may have set these for us
+SED="${SED-"sed"}"
 
 # Initialize
-GIT_DIR=".git"
 working_dir="$(pwd)"
 
 # Who am I?
@@ -142,7 +141,8 @@ if [ "x$git_found" = "xyes" ]; then
 fi
 
 git_repo=no
-if [ -e "$GIT_DIR/index" ]; then
+# "git-rev-parse --git-dir" since git-0.99.7
+if [ "x$(git-rev-parse --git-dir 2> /dev/null)" != "x" ]; then
     git_repo=yes
     if [ "x$git_found" = "xyes" ]; then
         git_shaid=`git-rev-parse HEAD | $SED -n 's/^\(.\{8\}\).*/\1/p'`

RadeonHD.am
@@ -13,7 +13,7 @@ git_version.stamp:
 			cp "$(srcdir)/git_version.h" "git_version.h"; \
 		fi; \
 	fi
-	env GIT_DIR="$(top_srcdir)/.git" $(GIT_VERSION_CMD) -k -s $(top_srcdir) -o git_version.h
+	$(GIT_VERSION_CMD) -k -s $(top_srcdir) -o git_version.h
 	@if test -s "$(srcdir)/git_version.h"; then \
 		if cmp "$(srcdir)/git_version.h" "git_version.h"; then :; \
 		else \

RadeonHD.tmpl
@@ -23,7 +23,7 @@ RHD_VERSION_DEFINES = \
  */
 GIT_VERSION_CMD = $(SHELL) $(TOP)/git_version.sh
 RHD_GIT_DEFINES = \
-	`env GIT_DIR="$(TOP)/.git" $(GIT_VERSION_CMD) -q -k -s $(TOP) -o git_version.h`
+	`$(GIT_VERSION_CMD) -q -k -s $(TOP) -o git_version.h`
 
 clean::
 	RemoveFile(git_version.h)

commit 0192e3463deb5b29765177d7344ad4b63eabd51f

Only accept the found git repo if it is in our top srcdir.
Fixes bug introduced in ff796eba:
In srcdir!=builddir builds, if builddir is a subdirectory of srcdir,
"make distcheck" exposes a conflict between the dist tarballs'
git_version.h and the new git_version.h erroneously created while
compiling the source tree from the dist tarball.

git_version.sh
@@ -104,6 +104,7 @@ else
 fi
 
 # Done with creating output files, so we can change to source dir
+abs_srcdir="$(cd "$srcdir" && pwd)"
 cd "$srcdir"
 
 # Write program header
@@ -142,7 +143,11 @@ fi
 
 git_repo=no
 # "git-rev-parse --git-dir" since git-0.99.7
-if [ "x$(git-rev-parse --git-dir 2> /dev/null)" != "x" ]; then
+git_repo_dir="$(git-rev-parse --git-dir 2> /dev/null)"
+abs_repo_dir="$(cd "$git_repo_dir" && pwd)"
+# Only accept the found git repo iff it is in our top srcdir, as determined
+# by comparing absolute pathnames creaged by running pwd in the respective dir.
+if [ "x$git_repo_dir" != "x" ] && [ "x${abs_repo_dir}" = "x${abs_srcdir}/.git" ]; then
     git_repo=yes
     if [ "x$git_found" = "xyes" ]; then
         # git-1.4 and probably earlier understand "git-rev-parse HEAD"