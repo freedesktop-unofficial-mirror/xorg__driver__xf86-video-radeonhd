dabe895ff402b262fe9078b6c2a8cc2a5dfaa504
Egbert Eich <eich@freedesktop.org>
2008-04-01 10:59:23 +0200

    Replace dummys with real register names for EXT_DIFF_POST_DIV_CNTL registers.

--- src/rhd_dig.c
+++ src/rhd_dig.c
@@ -841,8 +841,8 @@ struct DIGEncoder
 {
     Bool Stored;
 
-    CARD32 StoredReg420;
-    CARD32 StoredReg424;
+    CARD32 StoredRegExt1DiffPostDivCntl;
+    CARD32 StoredRegExt2DiffPostDivCntl;
     CARD32 StoredDIGClockPattern;
     CARD32 StoredLVDSDataCntl;
     CARD32 StoredTMDSPixelEncoding;
@@ -943,7 +943,7 @@ EncoderSet(struct rhdOutput *Output, struct rhdCrtc *Crtc, DisplayModePtr Mode)
 		    ? 0
 		    : RV62_LINK_STEER_SWAP), RV62_LINK_STEER_SWAP);
     } else { /* LVTMA */
-	RHDRegMask(Output, RV620_0424, 0, 0x100);
+	RHDRegMask(Output, RV620_EXT2_DIFF_POST_DIV_CNTL, 0, 0x1 << RV62_EXT2_DIFF_DRIVER_ENABLE_SHIFT);
     }
 
     if (Private->EncoderMode == LVDS)
@@ -994,11 +994,12 @@ EncoderPower(struct rhdOutput *Output, int Power)
 	case RHD_POWER_RESET:
 	case RHD_POWER_SHUTDOWN:
 	default:
-	    /* disable DIG */
+	    /* disable differential clock driver */
 	    if (Private->EncoderID == ENCODER_DIG1)
-		RHDRegMask(Output, RV620_0420, 0, 0xf00);
+		RHDRegMask(Output, RV620_EXT1_DIFF_POST_DIV_CNTL, 0, RV62_EXT1_DIFF_DRIVER_ENABLE);
 	    else
-		RHDRegMask(Output, RV620_0424, 0, 0x100);
+		RHDRegMask(Output, RV620_EXT2_DIFF_POST_DIV_CNTL, 0, 0x1 << RV62_EXT2_DIFF_DRIVER_ENABLE_SHIFT);
+	    /* disable DIG */
 	    RHDRegMask(Output, off + RV620_DIG1_CNTL, 0x0, 0x1010);
 	    RHDRegMask(Output, (Private->EncoderID == ENCODER_DIG2)
 		       ? RV620_DCCG_PCLK_DIGB_CNTL
@@ -1020,8 +1021,8 @@ EncoderSave(struct rhdOutput *Output)
 
     RHDFUNC(Output);
 
-    Private->StoredReg424          = RHDRegRead(Output, off + RV620_0424);
-    Private->StoredReg420          = RHDRegRead(Output, off + RV620_0420);
+    Private->StoredRegExt1DiffPostDivCntl          = RHDRegRead(Output, off + RV620_EXT1_DIFF_POST_DIV_CNTL);
+    Private->StoredRegExt2DiffPostDivCntl          = RHDRegRead(Output, off + RV620_EXT2_DIFF_POST_DIV_CNTL);
     Private->StoredDIGClockPattern = RHDRegRead(Output, off + RV620_DIG1_CLOCK_PATTERN);
     Private->StoredLVDSDataCntl    = RHDRegRead(Output, off + RV620_LVDS1_DATA_CNTL);
     Private->StoredDIGCntl         = RHDRegRead(Output, off + RV620_DIG1_CNTL);
@@ -1056,8 +1057,8 @@ EncoderRestore(struct rhdOutput *Output)
 	return;
     }
 
-    RHDRegWrite(Output, off + RV620_0424, Private->StoredReg424);
-    RHDRegWrite(Output, off + RV620_0420, Private->StoredReg420);
+    RHDRegWrite(Output, off + RV620_EXT1_DIFF_POST_DIV_CNTL, Private->StoredRegExt1DiffPostDivCntl);
+    RHDRegWrite(Output, off + RV620_EXT2_DIFF_POST_DIV_CNTL, Private->StoredRegExt2DiffPostDivCntl);
     /* reprogram all values but don't start the encoder, yet */
     RHDRegWrite(Output, off + RV620_DIG1_CNTL, Private->StoredDIGCntl & ~(CARD32)RV62_DIG_START);
     RHDRegWrite(Output, RV620_DCIO_LINK_STEER_CNTL, Private->StoredDCIOLinkSteerCntl);
