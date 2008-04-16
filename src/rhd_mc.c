27f5eb6e7e7127bf1dd25f6cb0ec17ddb8c90f94
Egbert Eich <eich@freedesktop.org>
2008-04-16 10:55:23 +0200

    Treat RS780 MC like R6xx for now.

--- src/rhd_mc.c
+++ src/rhd_mc.c
@@ -124,10 +124,14 @@ RHDSaveMC(RHDPtr rhdPtr)
     } else if (rhdPtr->ChipSet < RHD_R600) {
 	MC->FbLocation = RHDReadMC(rhdPtr, RS69_MCCFG_FB_LOCATION);
 	MC->HdpFbBase = RHDRegRead(rhdPtr, HDP_FB_LOCATION);
-    } else if (RHDFamily(rhdPtr->ChipSet) == RHD_FAMILY_RS780) {
+    }
+#if 0
+    else if (RHDFamily(rhdPtr->ChipSet) == RHD_FAMILY_RS780) {
  	MC->FbLocation = RHDReadMC(rhdPtr, RS78_MC_FB_LOCATION);
  	MC->HdpFbBase = RHDRegRead(rhdPtr, R6XX_HDP_NONSURFACE_BASE);
-    } else {
+    }
+#endif
+    else {
 	MC->FbLocation = RHDRegRead(rhdPtr, R6XX_MC_VM_FB_LOCATION);
 	/* RS780 uses the same register as R6xx */
 	MC->HdpFbBase = RHDRegRead(rhdPtr, R6XX_HDP_NONSURFACE_BASE);
@@ -167,10 +171,14 @@ RHDRestoreMC(RHDPtr rhdPtr)
     } else if (RHDFamily(rhdPtr->ChipSet) == RHD_FAMILY_RS690) {
 	RHDWriteMC(rhdPtr,  RS69_MCCFG_FB_LOCATION, MC->FbLocation);
 	RHDRegWrite(rhdPtr, HDP_FB_LOCATION, MC->HdpFbBase);
-    } else if (RHDFamily(rhdPtr->ChipSet) == RHD_FAMILY_RS780) {
+    }
+#if 0
+    else if (RHDFamily(rhdPtr->ChipSet) == RHD_FAMILY_RS780) {
  	RHDWriteMC(rhdPtr, RS78_MC_FB_LOCATION, MC->FbLocation);
  	RHDRegWrite(rhdPtr, R6XX_HDP_NONSURFACE_BASE, MC->HdpFbBase);
-    } else {
+    }
+#endif
+    else {
 	RHDRegWrite(rhdPtr, R6XX_MC_VM_FB_LOCATION, MC->FbLocation);
 	/* RS780 uses the same register as R6xx */
 	RHDRegWrite(rhdPtr, R6XX_HDP_NONSURFACE_BASE, MC->HdpFbBase);
@@ -232,7 +240,9 @@ RHDMCSetup(RHDPtr rhdPtr)
 		 fb_size,(unsigned int)fb_location_tmp);
 	RHDWriteMC(rhdPtr, RS69_MCCFG_FB_LOCATION, fb_location_tmp);
 	RHDRegWrite(rhdPtr, HDP_FB_LOCATION, fb_location_tmp & 0xFFFF);
-    } else if (RHDFamily(rhdPtr->ChipSet) == RHD_FAMILY_RS780) {
+    }
+#if 0
+    else if (RHDFamily(rhdPtr->ChipSet) == RHD_FAMILY_RS780) {
 	fb_location = RHDReadMC(rhdPtr, RS78_MC_FB_LOCATION);
 	fb_size = (fb_location >> 16) - (fb_location & 0xFFFF);
 	fb_location_tmp = rhdPtr->FbIntAddress >> 16;
@@ -246,7 +256,9 @@ RHDMCSetup(RHDPtr rhdPtr)
 	RHDWriteMC(rhdPtr, RS78_MC_FB_LOCATION, fb_location_tmp);
 	/* RS780 uses the same register as R6xx */
   	RHDRegWrite(rhdPtr, R6XX_HDP_NONSURFACE_BASE, hdp_fbbase_tmp);
-    } else {
+    }
+#endif
+    else {
 	fb_location = RHDRegRead(rhdPtr, R6XX_MC_VM_FB_LOCATION);
 	fb_size = (fb_location >> 16) - (fb_location & 0xFFFF);
 	fb_location_tmp = rhdPtr->FbIntAddress >> 24;
@@ -283,10 +295,14 @@ RHDMCIdle(RHDPtr rhdPtr, CARD32 count)
 	} else if (rhdPtr->ChipSet < RHD_R600) {
 	    if (RHDReadMC(rhdPtr, RS69_MC_SYSTEM_STATUS) & RS6X_MC_SEQUENCER_IDLE)
 		return TRUE;
-	} else if (RHDFamily(rhdPtr->ChipSet) == RHD_FAMILY_RS780) {
+	}
+#if 0
+	else if (RHDFamily(rhdPtr->ChipSet) == RHD_FAMILY_RS780) {
  	    if (RHDReadMC(rhdPtr, RS78_MC_SYSTEM_STATUS) & RS78_MC_SEQUENCER_IDLE)
  		return TRUE;
-	} else {
+	}
+#endif
+	else {
 	    if (!(RHDRegRead(rhdPtr, R6_MCLK_PWRMGT_CNTL) & R6_MC_BUSY))
 		return TRUE;
 	}
