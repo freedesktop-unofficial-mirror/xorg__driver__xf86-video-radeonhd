abf65fe4901f115c34ab418ebd98459988987d5f
Brad Smith <brad@comstyle.com>
2008-06-26 10:28:26 -0400

    RADEON: fix copy/paste error in accel code

--- src/radeon_exa_render.c
+++ src/radeon_exa_render.c
@@ -1991,7 +1991,7 @@ static void FUNC_NAME(RadeonCompositeTile)(PixmapPtr pDst,
 		 RADEON_CP_VC_CNTL_VTX_FMT_RADEON_MODE |
 		 (4 << RADEON_CP_VC_CNTL_NUM_SHIFT));
     } else*/ {
-	if (IS_R300_3D | IS_R500_3D)
+	if (IS_R300_3D || IS_R500_3D)
 	    BEGIN_RING(4 * vtx_count + 4);
 	else
 	    BEGIN_RING(4 * vtx_count + 2);
@@ -2004,7 +2004,7 @@ static void FUNC_NAME(RadeonCompositeTile)(PixmapPtr pDst,
     }
 
 #else /* ACCEL_CP */
-    if (IS_R300_3D | IS_R500_3D)
+    if (IS_R300_3D || IS_R500_3D)
 	BEGIN_ACCEL(2 + vtx_count * 4);
     else
 	BEGIN_ACCEL(1 + vtx_count * 4);
@@ -2045,7 +2045,7 @@ static void FUNC_NAME(RadeonCompositeTile)(PixmapPtr pDst,
 		xFixedToFloat(srcTopRight.x) / info->accel_state->texW[0],     xFixedToFloat(srcTopRight.y) / info->accel_state->texH[0]);
     }
 
-    if (IS_R300_3D | IS_R500_3D)
+    if (IS_R300_3D || IS_R500_3D)
 	/* flushing is pipelined, free/finish is not */
 	OUT_ACCEL_REG(R300_RB3D_DSTCACHE_CTLSTAT, R300_DC_FLUSH_3D);
 
@@ -2127,7 +2127,7 @@ static void FUNC_NAME(RadeonDoneComposite)(PixmapPtr pDst)
 
     ENTER_DRAW(0);
 
-    if (IS_R300_3D | IS_R500_3D) {
+    if (IS_R300_3D || IS_R500_3D) {
 	BEGIN_ACCEL(2);
 	OUT_ACCEL_REG(R300_RB3D_DSTCACHE_CTLSTAT, R300_RB3D_DC_FLUSH_ALL);
     } else
