73c131156b0beb9de214f68d08e5eddec46e8c2a
Alex Deucher <alexdeucher@gmail.com>
2009-01-14 18:01:43 -0500

    R6xx/R7xx: add chip specific SQ setup

--- src/r6xx_accel.c
+++ src/r6xx_accel.c
@@ -741,34 +741,123 @@ set_default_state(ScrnInfoPtr pScrn, drmBufPtr ib)
     // need to set stack/thread/gpr limits based on the asic
     // for now just set them low enough so any card will work
     // see r600_cp.c in the drm
-    if (rhdPtr->ChipSet <= RHD_RV770) {
-	    sq_conf.num_ps_gprs = 100;
-	    sq_conf.num_vs_gprs = 24;
-	    sq_conf.num_temp_gprs = 4;
-	    sq_conf.num_gs_gprs = 0;
-	    sq_conf.num_es_gprs = 0;
-	    sq_conf.num_ps_threads = 136;
-	    sq_conf.num_vs_threads = 48;
-	    sq_conf.num_gs_threads = 4;
-	    sq_conf.num_es_threads = 4;
-	    sq_conf.num_ps_stack_entries = 40;
-	    sq_conf.num_vs_stack_entries = 40;
-	    sq_conf.num_gs_stack_entries = 0;
-	    sq_conf.num_es_stack_entries = 0;
-    } else {
-	    sq_conf.num_ps_gprs = 192;
-	    sq_conf.num_vs_gprs = 56;
-	    sq_conf.num_temp_gprs = 4;
-	    sq_conf.num_gs_gprs = 0;
-	    sq_conf.num_es_gprs = 0;
-	    sq_conf.num_ps_threads = 180;
-	    sq_conf.num_vs_threads = 60;
-	    sq_conf.num_gs_threads = 0;
-	    sq_conf.num_es_threads = 0;
-	    sq_conf.num_ps_stack_entries = 256;
-	    sq_conf.num_vs_stack_entries = 256;
-	    sq_conf.num_gs_stack_entries = 0;
-	    sq_conf.num_es_stack_entries = 0;
+    switch (rhdPtr->ChipSet) {
+    case RHD_R600:
+	sq_conf.num_ps_gprs = 126;
+	sq_conf.num_vs_gprs = 126;
+	sq_conf.num_temp_gprs = 4;
+	sq_conf.num_gs_gprs = 0;
+	sq_conf.num_es_gprs = 0;
+	sq_conf.num_ps_threads = 140;
+	sq_conf.num_vs_threads = 44;
+	sq_conf.num_gs_threads = 4;
+	sq_conf.num_es_threads = 4;
+	sq_conf.num_ps_stack_entries = 128;
+	sq_conf.num_vs_stack_entries = 128;
+	sq_conf.num_gs_stack_entries = 0;
+	sq_conf.num_es_stack_entries = 0;
+	break;
+    case RHD_RV630:
+    case RHD_M76:
+    case RHD_RV635:
+    case RHD_M86:
+	sq_conf.num_ps_gprs = 100;
+	sq_conf.num_vs_gprs = 24;
+	sq_conf.num_temp_gprs = 4;
+	sq_conf.num_gs_gprs = 0;
+	sq_conf.num_es_gprs = 0;
+	sq_conf.num_ps_threads = 140;
+	sq_conf.num_vs_threads = 44;
+	sq_conf.num_gs_threads = 4;
+	sq_conf.num_es_threads = 4;
+	sq_conf.num_ps_stack_entries = 64;
+	sq_conf.num_vs_stack_entries = 64;
+	sq_conf.num_gs_stack_entries = 0;
+	sq_conf.num_es_stack_entries = 0;
+	break;
+    case RHD_RV610:
+    case RHD_M74:
+    case RHD_M72:
+    case RHD_RV620:
+    case RHD_RS780:
+    default:
+	sq_conf.num_ps_gprs = 100;
+	sq_conf.num_vs_gprs = 24;
+	sq_conf.num_temp_gprs = 4;
+	sq_conf.num_gs_gprs = 0;
+	sq_conf.num_es_gprs = 0;
+	sq_conf.num_ps_threads = 140;
+	sq_conf.num_vs_threads = 44;
+	sq_conf.num_gs_threads = 4;
+	sq_conf.num_es_threads = 4;
+	sq_conf.num_ps_stack_entries = 64;
+	sq_conf.num_vs_stack_entries = 64;
+	sq_conf.num_gs_stack_entries = 0;
+	sq_conf.num_es_stack_entries = 0;
+	break;
+    case RHD_RV670:
+    case RHD_R680:
+	sq_conf.num_ps_gprs = 94;
+	sq_conf.num_vs_gprs = 94;
+	sq_conf.num_temp_gprs = 4;
+	sq_conf.num_gs_gprs = 0;
+	sq_conf.num_es_gprs = 0;
+	sq_conf.num_ps_threads = 140;
+	sq_conf.num_vs_threads = 44;
+	sq_conf.num_gs_threads = 4;
+	sq_conf.num_es_threads = 4;
+	sq_conf.num_ps_stack_entries = 128;
+	sq_conf.num_vs_stack_entries = 128;
+	sq_conf.num_gs_stack_entries = 0;
+	sq_conf.num_es_stack_entries = 0;
+	break;
+    case RHD_RV770:
+    case RHD_M98:
+	sq_conf.num_ps_gprs = 126;
+	sq_conf.num_vs_gprs = 126;
+	sq_conf.num_temp_gprs = 4;
+	sq_conf.num_gs_gprs = 0;
+	sq_conf.num_es_gprs = 0;
+	sq_conf.num_ps_threads = 192;
+	sq_conf.num_vs_threads = 48;
+	sq_conf.num_gs_threads = 4;
+	sq_conf.num_es_threads = 4;
+	sq_conf.num_ps_stack_entries = 256;
+	sq_conf.num_vs_stack_entries = 256;
+	sq_conf.num_gs_stack_entries = 0;
+	sq_conf.num_es_stack_entries = 0;
+	break;
+    case RHD_RV730:
+    case RHD_M96:
+	sq_conf.num_ps_gprs = 100;
+	sq_conf.num_vs_gprs = 24;
+	sq_conf.num_temp_gprs = 4;
+	sq_conf.num_gs_gprs = 0;
+	sq_conf.num_es_gprs = 0;
+	sq_conf.num_ps_threads = 192;
+	sq_conf.num_vs_threads = 48;
+	sq_conf.num_gs_threads = 4;
+	sq_conf.num_es_threads = 4;
+	sq_conf.num_ps_stack_entries = 128;
+	sq_conf.num_vs_stack_entries = 128;
+	sq_conf.num_gs_stack_entries = 0;
+	sq_conf.num_es_stack_entries = 0;
+	break;
+    case RHD_RV710:
+	sq_conf.num_ps_gprs = 126;
+	sq_conf.num_vs_gprs = 126;
+	sq_conf.num_temp_gprs = 4;
+	sq_conf.num_gs_gprs = 0;
+	sq_conf.num_es_gprs = 0;
+	sq_conf.num_ps_threads = 140;
+	sq_conf.num_vs_threads = 44;
+	sq_conf.num_gs_threads = 4;
+	sq_conf.num_es_threads = 4;
+	sq_conf.num_ps_stack_entries = 128;
+	sq_conf.num_vs_stack_entries = 128;
+	sq_conf.num_gs_stack_entries = 0;
+	sq_conf.num_es_stack_entries = 0;
+	break;
     }
 
     sq_setup(pScrn, ib, &sq_conf);
