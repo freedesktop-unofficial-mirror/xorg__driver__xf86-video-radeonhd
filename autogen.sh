da5b9c2fb0338e616f5376b6e40b4531fa3d7e1c
Matthias Hopf <mhopf@suse.de>
2009-08-03 17:17:44 +0200

    Fix autoconf issues.
    Apparently, autoconf 2.64 has "interesting" issues with spaces in double
    quoted strings, as used in configure.ac. Change that to parantheses.
    Based on a patch by Jens Lody <radeonhd@jenslody.de>.
    
    Additionally, autogen.sh didn't --force so far.

--- autogen.sh
+++ autogen.sh
@@ -4,10 +4,10 @@ srcdir=`dirname "$0"`
 test -z "$srcdir" && srcdir=.
 
 ORIGDIR=`pwd`
-cd "$srcdir";
+cd "$srcdir"
 
 rm -rf autom4te.cache
-autoreconf -v --install || exit 1
+autoreconf --force -v --install || exit 1
 cd "$ORIGDIR" || exit $?
 
 "$srcdir"/configure --enable-maintainer-mode "$@"
