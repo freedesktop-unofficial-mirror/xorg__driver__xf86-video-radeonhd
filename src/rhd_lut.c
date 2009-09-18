freedesktop-unofficial-mirror/xorg__driver__xf86-video-radeonhd

commit bab7c5f97ddcd23772e6fdd66084f791c30218cd

LUT: Change internal functions to reflect hardware
Existing implementation of LUT setter uses a LoadPalette()-like
function signature, where as the actual hardware is more accurately
reflected by RandR style gamma setter.  On a RandR gamma adjustment
call, this results the following sequence of frivolous conversions:

  (u16*,u16*,u16*) -> LOCO* -> (u16*,u16*,u16*)

New implementation changes the LUT setter to accept a triplet of CARD16*,
and move the LoadPalette()-specific logic to RHDLoadPalette().

A new rhdLUTSetRow() function is added, which deals with setting only
specific rows of a LUT.  LUTxSet() previously made bad assumptions in
these cases.

src/rhd_lut.c
@@ -119,14 +119,16 @@ LUTxRestore(struct rhdLUT *LUT)
 }
 
 /*
+ * Load a new LUT
  *
+ * Assumes 256 rows of input. It's up to the caller to ensure there are exactly
+ * 256 rows of data, as that's what the hardware exepcts.
  */
 static void
-LUTxSet(struct rhdLUT *LUT, int numColors, int *indices, LOCO *colors)
+rhdLUTSet(struct rhdLUT *LUT, CARD16 *red, CARD16 *green, CARD16 *blue)
 {
-    ScrnInfoPtr pScrn = xf86Screens[LUT->scrnIndex];
     CARD16 RegOff;
-    int i, index, hw_index;
+    int i;
 
     LUT->Initialised = TRUE; /* thank you RandR */
 
@@ -153,66 +155,42 @@ LUTxSet(struct rhdLUT *LUT, int numColors, int *indices, LOCO *colors)
     RHDRegWrite(LUT, DC_LUT_RW_MODE, 0); /* table */
     RHDRegWrite(LUT, DC_LUT_WRITE_EN_MASK, 0x0000003F);
 
-    /* DC_LUT_RW_INDEX is incremented automatically when DC_LUT_30_COLOR
-     * is accessed; hw_index is used to track the value of DC_LUT_RW_INDEX
-     * so that we can properly handle the very unlikely case that the input
-     * table indexes are not monotonically increasing
-     */
-    switch (pScrn->depth) {
-    case 8:
-    case 24:
-    case 32:
-        RHDRegWrite(LUT, DC_LUT_RW_INDEX, 0);
-        hw_index = 0;
-	for (i = 0; i < numColors; i++) {
-            index = indices[i];
-            if (hw_index != index) {
-                RHDRegWrite(LUT, DC_LUT_RW_INDEX, index);
-                hw_index = index;
-            }
-	    RHDRegWrite(LUT, DC_LUT_30_COLOR, (colors[index].red << 20) |
-			(colors[index].green << 10) | (colors[index].blue));
-            hw_index++;
-	}
-	break;
-    case 16:
-        RHDRegWrite(LUT, DC_LUT_RW_INDEX, 0);
-        hw_index = 0;
-	for (i = 0; i < numColors; i++) {
-	    int j;
+    for (i = 0; i < 256; i++) {
+        RHDRegWrite(LUT, DC_LUT_30_COLOR,
+                    ((red[i] & 0xFFC0) << 14) | ((green[i] & 0xFFC0) << 4) | (blue[i] >> 6));
+    }
+}
 
-	    index = indices[i];
-            if (hw_index != 4 * index) {
-                RHDRegWrite(LUT, DC_LUT_RW_INDEX, 4 * index);
-                hw_index = 4 * index;
-            }
+/*
+ * Set specific rows of the LUT
+ *
+ * Assumes LUTs are already initialized to a sane state, and will only update
+ * specific rows.  Use ONLY when just specific rows need to be updated.
+ */
+static void
+rhdLUTSetRows(struct rhdLUT *LUT, int numColors, int *indices, LOCO *colors)
+{
+    CARD16 RegOff;
+    int i, index;
 
-	    for (j = 0; j < 4; j++) {
-		RHDRegWrite(LUT, DC_LUT_30_COLOR, (colors[index/2].red << 20) |
-			    (colors[index].green << 10) | (colors[index/2].blue));
-                hw_index++;
-            }
-	}
-	break;
-    case 15:
-        RHDRegWrite(LUT, DC_LUT_RW_INDEX, 0);
-        hw_index = 0;
-	for (i = 0; i < numColors; i++) {
-	    int j;
+    if (LUT->Id == RHD_LUT_A)
+	RegOff = RHD_REGOFFSET_LUTA;
+    else
+	RegOff = RHD_REGOFFSET_LUTB;
 
-	    index = indices[i];
-            if (hw_index != 8 * index) {
-                RHDRegWrite(LUT, DC_LUT_RW_INDEX, 8 * index);
-                hw_index = 8 * index;
-            }
+    if (LUT->Id == RHD_LUT_A)
+	RHDRegWrite(LUT, DC_LUT_RW_SELECT, 0);
+    else
+	RHDRegWrite(LUT, DC_LUT_RW_SELECT, 1);
 
-	    for (j = 0; j < 8; j++) {
-		RHDRegWrite(LUT, DC_LUT_30_COLOR, (colors[index].red << 20) |
-			    (colors[index].green << 10) | (colors[index].blue));
-                hw_index++;
-            }
-	}
-	break;
+    RHDRegWrite(LUT, DC_LUT_RW_MODE, 0); /* table */
+    RHDRegWrite(LUT, DC_LUT_WRITE_EN_MASK, 0x0000003F);
+
+    for (i = 0; i < numColors; i++) {
+        index = indices[i];
+        RHDRegWrite(LUT, DC_LUT_RW_INDEX, index);
+        RHDRegWrite(LUT, DC_LUT_30_COLOR,
+                    (colors[index].red << 20) | (colors[index].green) << 10) | (colors[index].blue);
     }
 }
 
@@ -234,7 +212,8 @@ RHDLUTsInit(RHDPtr rhdPtr)
 
     LUT->Save = LUTxSave;
     LUT->Restore = LUTxRestore;
-    LUT->Set = LUTxSet;
+    LUT->Set = rhdLUTSet;
+    LUT->SetRows = rhdLUTSetRows;
 
     rhdPtr->LUT[0] = LUT;
 
@@ -246,7 +225,8 @@ RHDLUTsInit(RHDPtr rhdPtr)
 
     LUT->Save = LUTxSave;
     LUT->Restore = LUTxRestore;
-    LUT->Set = LUTxSet;
+    LUT->Set = rhdLUTSet;
+    LUT->SetRows = rhdLUTSetRows;
 
     rhdPtr->LUT[1] = LUT;
 }
@@ -335,8 +315,7 @@ RHDLUTsDestroy(RHDPtr rhdPtr)
 void
 RHDLUTCopyForRR(struct rhdLUT *LUT)
 {
-    int indices[0x100];
-    LOCO colors[0x100];
+    CARD16 red[256], green[256], blue[256];
     CARD32 entry;
     int i;
 
@@ -349,50 +328,10 @@ RHDLUTCopyForRR(struct rhdLUT *LUT)
     else
 	RHDRegWrite(LUT, DC_LUT_READ_PIPE_SELECT, 0);
 
-    switch (xf86Screens[LUT->scrnIndex]->depth) {
-    case 8:
-    case 24:
-    case 32:
-	RHDRegWrite(LUT, DC_LUT_RW_INDEX, 0);
-
-	for (i = 0; i < 0x100; i++) {
-	    indices[i] = i;
-
-	    entry = RHDRegRead(LUT, DC_LUT_30_COLOR);
-
-	    colors[i].red = (entry >> 20) & 0x3FF;
-	    colors[i].green = (entry >> 10) & 0x3FF;
-	    colors[i].blue = (entry) & 0x3FF;
-	}
-	LUT->Set(LUT, 0x100, indices, colors);
-	break;
-    case 16:
-	for (i = 0; i < 0x40; i++) {
-	    indices[i] = i;
-
-	    RHDRegWrite(LUT, DC_LUT_RW_INDEX, 4 * i);
-
-	    entry = RHDRegRead(LUT, DC_LUT_30_COLOR);
-
-	    colors[i / 2].red = (entry >> 20) & 0x3FF;
-	    colors[i].green = (entry >> 10) & 0x3FF;
-	    colors[i / 2].blue = (entry) & 0x3FF;
-	}
-	LUT->Set(LUT, 0x40, indices, colors);
-	break;
-    case 15:
-	for (i = 0; i < 0x20; i++) {
-	    indices[i] = i;
-
-	    RHDRegWrite(LUT, DC_LUT_RW_INDEX, 8 * i);
-
-	    entry = RHDRegRead(LUT, DC_LUT_30_COLOR);
-
-	    colors[i].red = (entry >> 20) & 0x3FF;
-	    colors[i].green = (entry >> 10) & 0x3FF;
-	    colors[i].blue = (entry) & 0x3FF;
-	}
-	LUT->Set(LUT, 0x20, indices, colors);
-	break;
+    for (i = 0; i < 256; i++) {
+        entry = RHDRegRead(LUT, DC_LUT_30_COLOR);
+        red[i] = (entry >> 14) & 0xFFC0;
+        green[i] = (entry >> 4) & 0xFFC0;
+        blue[i] = (entry << 6) & 0xFFC0;
     }
 }

src/rhd_driver.c
@@ -1686,13 +1686,70 @@ RHDLoadPalette(ScrnInfoPtr pScrn, int numColors, int *indices, LOCO *colors,
     RHDPtr rhdPtr = RHDPTR(pScrn);
     struct rhdCrtc *Crtc;
 
-    Crtc = rhdPtr->Crtc[0];
-    if ((pScrn->scrnIndex == Crtc->scrnIndex) && Crtc->Active)
-	Crtc->LUT->Set(Crtc->LUT, numColors, indices, colors);
+    CARD16 red[256], green[256], blue[256];
+    int i, index, j, n;
+    Bool partial_table = FALSE;
 
-    Crtc = rhdPtr->Crtc[1];
-    if ((pScrn->scrnIndex == Crtc->scrnIndex) && Crtc->Active)
-	Crtc->LUT->Set(Crtc->LUT, numColors, indices, colors);
+    switch (pScrn->depth) {
+    case 8:
+    case 24:
+    case 32:
+        if (numColors < 256) {
+            partial_table = TRUE;
+            break;
+        }
+        for (i = 0; i < numColors; i++) {
+            index = indices[i];
+            red[index] = colors[index].red << 6;
+            green[index] = colors[index].green << 6;
+            blue[index] = colors[index].blue << 6;
+        }
+        break;
+    case 16:
+        if (numColors < 64) {
+            partial_table = TRUE;
+            break;
+        }
+        /* 6 bits of green, 5 bits of red and blue each */
+        for (i = 0; i < numColors; i++) {
+            index = indices[i];
+            n = index * 4;
+            for (j = 0; j < 4; j++) {
+                red[n + j] = colors[index/2].red << 6;
+                green[n + j] = colors[index].green << 6;
+                blue[n + j] = colors[index/2].blue << 6;
+            }
+        }
+        break;
+    case 15:
+        if (numColors < 32) {
+            partial_table = TRUE;
+            break;
+        }
+        /* 5 bits each */
+        for (i = 0; i < numColors; i++) {
+            int j, n;
+
+            index = indices[i];
+            n = index * 8;
+            for (j = 0; j < 8; j++) {
+                red[n + j] = colors[index].red << 6;
+                green[n + j] = colors[index].green << 6;
+                blue[n+ j] = colors[index].blue << 6;
+            }
+        }
+        break;
+    }
+
+    for (i = 0; i < 2; i++) {
+        Crtc = rhdPtr->Crtc[i];
+        if ((pScrn->scrnIndex == Crtc->scrnIndex) && Crtc->Active) {
+            if (!partial_table)
+                Crtc->LUT->Set(Crtc->LUT, red, green, blue);
+            else
+                Crtc->LUT->SetRows(Crtc->LUT, numColors, indices, colors);
+        }
+    }
 }
 
 /*

src/rhd_lut.h
@@ -36,7 +36,8 @@ struct rhdLUT {
 
     void (*Save) (struct rhdLUT *LUT);
     void (*Restore) (struct rhdLUT *LUT);
-    void (*Set) (struct rhdLUT *LUT, int numColors, int *indices, LOCO *colors);
+    void (*Set) (struct rhdLUT *LUT, CARD16 *red, CARD16 *green, CARD16 *blue);
+    void (*SetRows) (struct rhdLUT *LUT, int numColors, int *indices, LOCO *colors);
 
     /* because RandR does not specifically initialise a gamma ramp when
        setting up a CRTC */

src/rhd_randr.c
@@ -486,34 +486,23 @@ rhdRRCrtcCommit(xf86CrtcPtr crtc)
     RHDDebugRandrState(rhdPtr, rhdCrtc->Name);
 }
 
-/*
- * They just had to do NIH again here: Old X functionality provides a size, a
- * list of indices, and a table of RGB unsigned shorts. RandR provides what
- * is below. Apart from horribly breaking any attempt at being backwards
- * compatible, this also pretty much rules out the usage of indexed colours, as
- * each time even a single colour is changed an entirely new table has to be
- * uploaded. Just cute. -- libv.
- */
 static void
 rhdRRCrtcGammaSet(xf86CrtcPtr crtc, CARD16 *red, CARD16 *green, CARD16 *blue,
 		  int size)
 {
     struct rhdCrtc *rhdCrtc = ((struct rhdRandrCrtc *)(crtc->driver_private))->rhdCrtc;
-    int indices[0x100]; /* would RandR use a size larger than 256? */
-    LOCO colors[0x100];
-    int i;
-
     RHDDebug(rhdCrtc->scrnIndex, "%s: %s.\n", __func__, rhdCrtc->Name);
 
-    /* thanks so very much */
-    for (i = 0; i < size; i++) {
-	indices[i] = i;
-	colors[i].red = red[i] >> 6;
-	colors[i].green = green[i] >> 6;
-	colors[i].blue = blue[i] >> 6;
+    if (size < 256) {
+        xf86DrvMsg(rhdCrtc->scrnIndex, X_ERROR,
+                   "LUT: only %d rows of LUT given, when 256 expected; aborting", size);
+        return;
+    } else if (size > 256) {
+        xf86DrvMsg(rhdCrtc->scrnIndex, X_WARNING,
+                   "LUT: %d rows of LUT given, when 256 expected; some rows are ignored", size);
     }
 
-    rhdCrtc->LUT->Set(rhdCrtc->LUT, size, indices, colors);
+    rhdCrtc->LUT->Set(rhdCrtc->LUT, red, green, blue);
 }
 
 /* Dummy, because not tested for NULL */

commit b7a50a6f00bb6d207f5240f00cf83fbbd56bbf60

Fix RHDRegWrite macro invocation breakage
This appears to be an obvious fix to a bug introduced with bab7c5f9.

Neither ignoring the blue component nor discarding the calculation
does made sense to me, and the obvious explanation for that would
be a misplaced parenthesis.

src/rhd_lut.c
@@ -191,7 +191,7 @@ rhdLUTSetRows(struct rhdLUT *LUT, int numColors, int *indices, LOCO *colors)
         index = indices[i];
         RHDRegWrite(LUT, DC_LUT_RW_INDEX, index);
         RHDRegWrite(LUT, DC_LUT_30_COLOR,
-                    (colors[index].red << 20) | (colors[index].green) << 10) | (colors[index].blue);
+                    (colors[index].red << 20) | (colors[index].green << 10) | (colors[index].blue));
     }
 }
 