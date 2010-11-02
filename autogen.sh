231683e2f111bb064125f64f2da797d744cde7fa
SPIGOT <root@jerkcity.com>
2010-11-01 21:21:14 -0700

    PERHAPS BONGHITS WILL FIX MY MAKEFILE
    Signed-off-by: SPIGOT <root@jerkcity.com>

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,3 @@
 #! /bin/sh
 
-srcdir=`dirname "$0"`
-test -z "$srcdir" && srcdir=.
-
-ORIGDIR=`pwd`
-cd "$srcdir"
-
-rm -rf autom4te.cache
-autoreconf --force -v --install || exit 1
-cd "$ORIGDIR" || exit $?
-
-"$srcdir"/configure --enable-maintainer-mode "$@"
+echo "It's dead, Jim."
